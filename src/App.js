3f98e533029d6437ab335ba3d2f1f93fa201db27
OritLeshem <azmono@gmail.com>
2021-09-01 21:10:37 -0400

    Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 import Header from "./Header";
 import SideBar from "./SideBar";
+import Mail from "./Mail";
+import EmailList from "./EmailList";
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <SideBar />
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <div className="app_body">
+          <SideBar />
+          <Switch>
+            <Route path="/mail">
+              <Mail />
+            </Route>
+            <Route path="/">
+              <EmailList />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/EmailList.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./EmailList.css";
+
+function EmailList() {
+  return <div className="emailList">email list</div>;
+}
+
+export default EmailList;
