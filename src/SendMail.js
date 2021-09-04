dbd294391dff86508bd23f7c5b11647d8f3a476d
OritLeshem <azmono@gmail.com>
2021-09-03 21:18:29 -0400

    new mail

--- src/SendMail.js
+++ src/SendMail.js
@@ -1,8 +1,73 @@
+import CloseIcon from "@material-ui/icons/Close";
 import React from "react";
 import "./SendMail.css";
-
+import { Button } from "@material-ui/core";
+import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
+import { closeSendMessage } from "./features/mailSlice";
 function SendMail() {
-  return <div className="sendMail"></div>;
+  const dispatch = useDispatch();
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (formData) => {
+    console.log(formData);
+  };
+  return (
+    <div className="sendMail">
+      <div className="sendMail__header">
+        <h3>New message</h3>
+        <CloseIcon
+          onClick={() => {
+            dispatch(closeSendMessage());
+          }}
+          className="sendMail__close"
+        />
+      </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          name="to"
+          placeholder="To"
+          type="email"
+          {...register("to", { required: true })}
+        />
+        {errors.to && <p className="sendMail__error">To is required</p>}
+
+        <input
+          name="subject"
+          placeholder="Subject"
+          type="text"
+          {...register("subject", { required: true })}
+        />
+        {errors.subject && (
+          <p className="sendMail__error">Subject is required</p>
+        )}
+        <input
+          name="message"
+          placeholder="Message"
+          type="text"
+          className="sendMail__message"
+          {...register("message", { required: true })}
+        />
+        {errors.message && (
+          <p className="sendMail__error">Message is required</p>
+        )}
+        <div className="sendMail__options">
+          <Button
+            className="sendMail__send"
+            variant="contained"
+            color="primary"
+            type="submit"
+          >
+            Send
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default SendMail;
