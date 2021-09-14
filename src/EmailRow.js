a595be5239bb556bc306b5e61b1c9a2dd8223d3f
OritLeshem <azmono@gmail.com>
2021-09-14 13:28:53 -0400

    show selected email

--- src/EmailRow.js
+++ src/EmailRow.js
@@ -5,11 +5,26 @@ import Checkbox from "@material-ui/core/Checkbox";
 import StarBorderIcon from "@material-ui/icons/StarBorder";
 import LabelImportantIcon from "@material-ui/icons/LabelImportant";
 import { useHistory } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { selectMail } from "./features/mailSlice";
 
 function EmailRow({ id, title, subject, description, time }) {
   const history = useHistory();
+  const dispatch = useDispatch();
+  const openMail = () => {
+    dispatch(
+      selectMail({
+        id,
+        title,
+        subject,
+        description,
+        time,
+      })
+    );
+    history.push("/mail");
+  };
   return (
-    <div onClick={() => history.push("/mail")} className="emailRow">
+    <div onClick={openMail} className="emailRow">
       <div className="emailRow__options">
         <Checkbox />
         <IconButton>
--- src/Mail.js
+++ src/Mail.js
@@ -14,9 +14,13 @@ import { useHistory } from "react-router-dom";
 import UnfoldMoreIcon from "@material-ui/icons/UnfoldMore";
 import PrintIcon from "@material-ui/icons/Print";
 import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import { selectOpenMail } from "./features/mailSlice";
+import { useSelector } from "react-redux";
 
 function Mail() {
   const history = useHistory();
+  const selectedMail = useSelector(selectOpenMail);
+
   return (
     <div className="mail">
       <div className="mail__tools">
@@ -67,13 +71,13 @@ function Mail() {
       </div>
       <div className="mail__body">
         <div className="mail__bodyHeader">
-          <h2>subject</h2>
+          <h2>{selectedMail?.subject}</h2>
           <LabelImportantIcon className="mail__important" />
-          <p>title</p>
-          <p className="mail__time">10pm</p>
+          <p>{selectedMail?.title}</p>
+          <p className="mail__time">{selectedMail?.time}</p>
         </div>
         <div className="mail__message">
-          <p>this is a message</p>
+          <p>{selectedMail?.description}</p>
         </div>
       </div>
     </div>
